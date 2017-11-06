db8e1c52f2f54fed3ac3d6e26e98ba026e91972f
Anton Kostin <schnibba@gmail.com>
2017-11-06 15:03:33 +0300

    Fix form and add padding to agima logo.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024165400) do
+ActiveRecord::Schema.define(version: 20171106115907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171024165400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "attachments"
+    t.string "message"
   end
 
   add_foreign_key "attachments", "requests"
