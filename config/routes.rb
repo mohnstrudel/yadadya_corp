a6fbbc808181a7991bead72e96690154626007ed
Anton Kostin <schnibba@gmail.com>
2018-05-15 14:47:33 +0300

    Добавлена ссылка на услуги.

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
     get 'about', to: 'static_pages#about'
     get 'contact', to: 'static_pages#contact'
     
+    get 'services', to: 'services#services'
+
     scope 'services' do
       get '', to: 'services#services'
       get 'ecom', to: 'services#ecom'
