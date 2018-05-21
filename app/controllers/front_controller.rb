c5d6b935a7e9efef26990b468de1a889eead2eab
Anton Kostin <schnibba@gmail.com>
2018-05-21 18:37:04 +0300

    Force ssl and implement 301 redirect.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,3 +1,13 @@
 class FrontController < ApplicationController
   layout 'front'
+
+  before_action :redirect_if_old
+
+  protected
+
+  def redirect_if_old
+    if request.host == 'yadadya.com'
+      redirect_to "#{request.protocol}atomi-digital.ru#{request.fullpath}", :status => :moved_permanently 
+    end
+  end
 end
